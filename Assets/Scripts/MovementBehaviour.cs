dcfc4d7c4c54b7e30a164cf3256ec69567c8880e
FacuSalco <101563649+FacuSalco@users.noreply.github.com>
2022-07-12 09:40:21 -0300

    SimpleCharacterMovement
    Movimiento simple del personaje para pruebas

--- Assets/Scripts/MovementBehaviour.cs
+++ Assets/Scripts/MovementBehaviour.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class MovementBehaviour : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hola");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Chau");
+        
     }
 }
