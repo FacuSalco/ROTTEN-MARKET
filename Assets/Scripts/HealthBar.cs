f5ac76e879050ed0a00d806f0d3d7e5f6aa1e5d0
FacuSalco <facusalco26@gmail.com>
2022-07-12 21:36:52 -0300

    HealthBarDone
    Hice la barra de salud del player

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image barraVida;
+    public float vidaActual;
+    public float vidaMaxima;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        barraVida.fillAmount = vidaActual / vidaMaxima;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "5-Damage")
+        {
+            vidaActual -= 5;
+            Debug.Log("-5 vida");
+        }
+    }
+
+}
