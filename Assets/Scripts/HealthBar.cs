547dcbf46b0d676eba246788a30a07c72c19e393
FacuSalco <facusalco26@gmail.com>
2022-07-14 18:29:04 -0300

    Third person movement 2.0
    Hice el movimiento del personaje, interactua con rigidbodies, tiene salto, gravedad. Ahora tiene una camara simple, despues la mejoro

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -7,20 +7,21 @@ public class HealthBar : MonoBehaviour
 {
     public Image barraVida;
     public float vidaActual;
-    public float vidaMaxima;
+    public int vidaMaxima;
     public Text vidaActualtxt;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        vidaActual = vidaMaxima;
     }
 
     // Update is called once per frame
     void Update()
     {
+        int iVidaActual = (int)System.Math.Floor(vidaActual); //Convertir el float a int
         barraVida.fillAmount = vidaActual / vidaMaxima;
-        vidaActualtxt.text = (vidaActual + "%");
+        vidaActualtxt.text = (iVidaActual + "%");
     }
 
     void OnCollisionEnter(Collision col)
