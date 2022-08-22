65a6b2ab2149204c6b26d6079193a926dc7c6986
FacuSalco <facusalco26@gmail.com>
2022-08-22 14:30:43 -0300

    Pause menu in progres
    Estoy haciendo el menu de pausa pero por ahora sigue en progreso

---
+++ Assets/Scripts/PauseBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseBehaviour : MonoBehaviour
+{
+    public Canvas menuPausa;
+    bool paused;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Pause();
+        }
+
+        if (paused == true)
+        {
+            menuPausa.enabled = true;
+        }
+
+        else
+        {
+            menuPausa.enabled = false;
+        }
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        
+        if (paused == false)
+        {
+            paused = true;
+        }
+        if (paused == true)
+        {
+            paused = false;
+        }
+    }
+}
+
+//Revisar que no estaria andando sacar pausa
