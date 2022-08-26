968eaf0d33351fe84941b478275942358e7d3148
FacuSalco <facusalco26@gmail.com>
2022-08-26 14:28:38 -0300

    Made Configs
    Empece a hacer para que el chabon pueda editar la sensibilidad

---
+++ Assets/Scripts/ChangeConfigs.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class ChangeConfigs : MonoBehaviour
+{
+    public CinemachineFreeLook cineCam;
+    public float sensitivityX, sensitivityY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //HACER LA BARRITA PARA QUE EL CHABON PUEDA ELEGIR LA SENSIBILIDAD CAMBIANDO LAS VARIABLES ESTAS DE SENSITIVITI
+        cineCam.m_XAxis.m_MaxSpeed = 30 * sensitivityX;
+        cineCam.m_YAxis.m_MaxSpeed = sensitivityY / 3;
+    }
+}
