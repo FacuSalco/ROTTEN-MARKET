bd37324865bdbeb482480713f926c0531353c784
FacuSalco <facusalco26@gmail.com>
2022-08-29 14:32:42 -0300

    CoinSystemCreated
    Hice el sistema de recoleccion de monedas y que se guarden a pesar de que deje de jugar

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "PlayerData", menuName = "PlayerData")]
+public class PlayerData : ScriptableObject
+{
+    public int cantMonedas;
+}
