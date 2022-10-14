5f1af92660b353845088b8a6205a72e3cc1d03fc
CiroVilmer <101563425+CiroVilmer@users.noreply.github.com>
2022-10-14 14:22:28 -0300

    Small Change to NPC

--- Assets/Scripts/NPC/npcScriptableObject.cs
+++ Assets/Scripts/NPC/npcScriptableObject.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NpcData", menuName = "NpcData")]
 public class npcScriptableObject : ScriptableObject
 {
+    [Header("Dialogues")]
+
     public string[] dialoguesBeforeMission;
     public string mission;
     public string[] dialoguesAfterMission;
+
+    [Header("MissionState")]
+
     public bool hasAcceptedMission;
     public bool hasDoneMission;
     public bool hasTalked;
+
+    [Header("Rewards")]
+
     public int coinReward;
 }
