7ba5ea24a2ac5673ef66e22109de121966ede440
CiroVilmer <101563425+CiroVilmer@users.noreply.github.com>
2022-10-26 12:00:01 -0300

    Started doing NPC killing mission

--- Assets/Scripts/NPC/Scripts/MissionHandler.cs
+++ Assets/Scripts/NPC/Scripts/MissionHandler.cs
@@ -5,4 +5,5 @@ using UnityEngine;
 public class MissionHandler : MonoBehaviour
 {
     public bool IsOnMission = false;
+    public bool IsOnKillingMission = false;
 }
---
+++ Assets/Scripts/NPC/Scripts/MissionKillingNPC.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionKillingNPC : MonoBehaviour
+{
+    public int EnemyKillAmount;
+    private int EnemyKillAmountDelta;
+    public int EnemyKillCount;
+    private bool CompleteMissionOnce = true;
+
+    private MissionHandler MissionHand;
+    private NpcDialgueManager NpcManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MissionHand = GameObject.Find("[MISSION-MANAGER]").GetComponent<MissionHandler>();
+        NpcManager = GetComponent<NpcDialgueManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MissionHand.IsOnKillingMission)
+        {
+            if(EnemyKillCount == EnemyKillAmount)
+            {
+                if (CompleteMissionOnce)
+                {
+                    NpcManager.CompleteMission();
+                    CompleteMissionOnce = false;
+                }
+            }
+
+            if (NpcManager.hasFailedMission)
+            {
+                EnemyKillCount = 0;
+            }
+        }
+    }
+}
