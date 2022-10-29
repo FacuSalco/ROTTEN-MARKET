e630cc0b916bd1d931f77f8da0c208b85da6e9c2
FacuSalco <facusalco26@gmail.com>
2022-10-29 12:48:03 -0300

    Added Fade Between Scenes, Freezer Level entrance and commented the Persstent object sciript
    Habia un problema y el script de persistent object tipo que destruida todo al inicio de la escena y solo quedaba [ESTOS] y no se podia jugar

---
+++ Assets/Scripts/EnterFreezerLevel.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnterFreezerLevel : MonoBehaviour
+{
+    bool isNear;
+    Fade Fade;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Fade = GameObject.Find("Panel").GetComponent<Fade>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && isNear) //Entra al nivel Freezer
+        {
+            Fade.FadeOut();
+            Invoke("ChangeScene", 2);
+        }
+    }
+    
+    private void OnTriggerEnter(Collider col)
+    {
+
+        if (col.gameObject.tag == "Player")
+        {
+            isNear = true;
+            gameObject.GetComponentInChildren<Canvas>().enabled = true;
+        }
+    }
+    
+    void OnTriggerExit (Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            isNear = false;
+            gameObject.GetComponentInChildren<Canvas>().enabled = false;
+        }
+    }
+    
+    void ChangeScene()
+    {
+        SceneManager.LoadScene("FreezerScene");        
+    }
+
+}
+
