5e67cd5da74fa1480d52223d5c61207ea8db27dc
CiroVilmer <101563425+CiroVilmer@users.noreply.github.com>
2022-11-05 16:22:46 -0300

    Continued snow level requirements
    Termine haciendo un script distinto para los npcs que solo hablen, porque sino iba a tener que cambiar muchas cosas alpedo. Funciona bien, y ahora tengo que hacer que se pueda entrar al nivel completando las misiones.

---
+++ Assets/Scripts/NPC/Tnpc Scripts/TalkingNpc.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TalkingNpc : MonoBehaviour
+{
+    public GameObject NpcCanvas;
+    public GameObject DialogueCanvas;
+    public GameObject MissionCanvas;
+    public TextMeshProUGUI DialogueText;
+    public TextMeshProUGUI NextText;
+
+    public string[] Dialogues;
+
+    private int DialogueCounter = 0;
+    private bool playerOnRange = false;
+    private bool doDesactivateOnce = true;
+    private bool HasTalked = false;
+    private string pressE = "Presione ¨E¨ para continuar";
+
+    private Animator NpcAnimator;
+    private MissionHandler MissionHand;
+    private MissionStateChecker MissionChecker;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MissionHand = GameObject.Find("[MISSION-MANAGER]").GetComponent<MissionHandler>();
+        MissionChecker = MissionHand.GetComponent<MissionStateChecker>();
+        NpcAnimator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerOnRange && !MissionHand.IsOnMission)
+        {
+            NpcCanvas.SetActive(true);
+            NextText.enabled = true;
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (DialogueCounter <= Dialogues.Length)
+                {
+                    DialogueCounter++;
+                }
+
+            }
+
+            if (DialogueCounter >= Dialogues.Length)
+            {
+                HasTalked = true;
+            }
+
+            if (!HasTalked)
+            {
+                DialogueText.text = Dialogues[DialogueCounter];
+
+                NextText.text = pressE;
+            }
+
+            if (HasTalked)
+            {
+                NextText.text = "";
+            }
+
+        }
+        else
+        {
+            if (doDesactivateOnce)
+            {
+                NpcCanvas.SetActive(false);
+                doDesactivateOnce = false;
+            }
+        }
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerOnRange = true;
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerOnRange = false;
+            doDesactivateOnce = true;
+            HasTalked = false;
+            DialogueCounter = 0;
+        }
+    }
+
+}
